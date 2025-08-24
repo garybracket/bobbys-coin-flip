3b1f64518b6d2b5777baa39695e097974a340899
garybracket <garybracket@hotmail.com>
2025-08-24 15:13:17 -0400

    Add flip test functionality to debug game updates

--- api/test.js
+++ api/test.js
@@ -3,7 +3,7 @@ const database = require('./_lib/database');
 module.exports = async (req, res) => {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   
   if (req.method === 'OPTIONS') {
@@ -11,6 +11,29 @@ module.exports = async (req, res) => {
   }
   
   try {
+    const { action } = req.query;
+    
+    if (action === 'flip-test') {
+      // Test the updateUserAfterFlip function specifically
+      const testStats = {
+        newCoins: 120,
+        newXp: 400,
+        newLevel: 15,
+        newWins: 33,
+        newLosses: 30,
+        newGamesPlayed: 63,
+        newWinStreak: 3,
+        newBestStreak: 6
+      };
+      
+      const result = await database.updateUserAfterFlip('Garybracket', testStats);
+      return res.status(200).json({ 
+        success: true, 
+        message: 'Flip test completed',
+        updateResult: result
+      });
+    }
+    
     // Test database connection
     const result = await database.testConnection();
     res.status(200).json({ 
